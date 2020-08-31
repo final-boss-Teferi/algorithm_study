3190bcde6177042f7797fded473fdb81ce3d7cea
KJH <KJH@users.noreply.github.com>
2020-08-31 13:22:20 +0900

    1_basic_algorithm ex15

--- 1_basic_algorithm/ex15/rectangle.c
+++ 1_basic_algorithm/ex15/rectangle.c
@@ -7,3 +7,33 @@
 // *******
 // *******
 // *******
+
+#include <stdio.h>
+
+int main(void)
+{
+	int col, row;
+
+	printf("직사각형을 출력합니다.\n");
+	do
+	{
+		printf("높이 : ");
+		scanf("%d", &col);
+		if (col <= 0)
+			printf("양의 정수만 입력해주세요.\n");
+	} while (col <= 0);
+	do
+	{
+		printf("너비 : ");
+		scanf("%d", &row);
+		if (row <= 0)
+			printf("양의 정수만 입력해주세요.\n");
+	} while (row <= 0);
+
+	for (int i = 0; i < col; i++)
+	{
+		for (int j = 0; j < row; j++)
+			printf("*");
+		printf("\n");
+	}
+}
